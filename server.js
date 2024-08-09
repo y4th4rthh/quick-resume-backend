f3a4a68d3726a2c03ba14fe1bdf6d10183f9f1b0
Patel Yatharth <114477915+y4th4rthh@users.noreply.github.com>
2024-08-10 00:16:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-const mongoURI = 'mongodb+srv://yatharthpatel014:yatharth@cluster0.iq2m5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const mongoURI = 'const mongoURI = 'mongodb+srv://yatharthpatel014:yatharth@cluster0.iq2m5.mongodb.net/ResumeData?retryWrites=true&w=majority&tls=true';
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
